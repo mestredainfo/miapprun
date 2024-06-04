26d4d55584453829a99385887eb0943a8543599f
mestredainfo <>
2024-06-04 14:55:17 -0300

    Add files project

---
+++ linux/miapp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2004-2024 Murilo Gomes Julio
+# SPDX-License-Identifier: GPL-2.0-only
+
+# Organização: Mestre da Info
+# Site: https://linktr.ee/mestreinfo
+
+import sys
+import os
+from subprocess import Popen,PIPE
+proc = Popen(["/opt/miapp/miapp", os.path.dirname(sys.executable)])
+sys.exit()
