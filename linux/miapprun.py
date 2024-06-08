14ec6152960ab1117c2ca17aa477c7eca1c71b74
mestredainfo <>
2024-06-08 18:22:56 -0300

    Add verificação se o MIApp está instalado ou não, caso não esteja, exibe um aviso

--- linux/miapprun.py
+++ linux/miapprun.py
@@ -8,6 +8,23 @@
 
 import sys
 import os
+import webbrowser
+import tkinter
+import tkinter.messagebox
 from subprocess import Popen,PIPE
-proc = Popen(["/opt/miapp/miapp", os.path.dirname(sys.executable)])
+
+def confirm():
+    window = tkinter.Tk()
+    window.wm_withdraw()
+    message = tkinter.messagebox.askyesno(title="Requer MIApp", message="Para executar esse software você precisa do MIApp instalado, deseja baixar o MIApp agora?")
+
+    if message:
+        webbrowser.open_new('https://mestredainfo.wordpress.com/miapp/')
+
+
+if os.path.isfile("/opt/miapp/miapp"):
+    proc = Popen(["/opt/miapp/miapp", os.path.dirname(sys.executable)])
+else:
+    confirm()
+
 sys.exit()
