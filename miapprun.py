1a1d70003efb4bfa0f9f828b5796087662839bc3
mestredainfo <>
2024-06-08 23:46:30 -0300

    Add verificação de plataforma e se o MIApp foi encontrado, se for executa o software.

--- miapprun.py
+++ miapprun.py
@@ -8,31 +8,15 @@
 
 import sys
 import os
-import webbrowser
 import platform
-import locale
 from subprocess import Popen,PIPE
 
-milang, _ = locale.getlocale()
-milangpt = milang.split('_')[0]
-    
-def miConfirm():    
-    if milangpt == "pt":    
-    	sys.stdout.write("\nO MIApp não foi encontrado!\n\nO " + os.path.basename(sys.executable) + " requer o MIApp instalado, deseja baixar agora? [s/n]")
-    else:
-    	sys.stdout.write("\nMIApp was not found!\n\n" + os.path.basename(sys.executable) + " requires MIApp installed, want to download now? [y/n]")
-    	
-    miChoice = input().lower()
-    if miChoice == 'y' or miChoice == 's':
-        webbrowser.open_new('https://mestredainfo.wordpress.com/miapp/')
-
-miAppFile = "/opt/miapp/miapp"
-if (platform.system() == "Windows"):
-    miAppFile = "C:\\\\miapp\\\\miapp.exe"
+if (platform.system() == "Linux"):
+    miAppFile = "/opt/miapp/miapp"
+else:
+	miAppFile = "C:\\\\miapp\\\\miapp.exe"
 
 if os.path.isfile(miAppFile):
     miProc = Popen([miAppFile, os.path.dirname(sys.executable)])
-else:
-    miConfirm()
 
 sys.exit()
